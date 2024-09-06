e5f041b766f21eb12fcbbcbeb1884d41eaa7b2c3
srivardhan-kondu <srivardhan.kondu@gmail.com>
2024-09-06 23:51:11 +0530

    Note book folder created, EDA and Model Training is done

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 def error_message_details(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -17,3 +17,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by  Zero")
+        raise CustomException(e,sys)
